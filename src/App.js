635dc7cfdb69cc5370439e136e9ee357effd010e
GabrielBecher <gabriel.abecher@hotmail.com>
2024-09-13 17:59:21 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import react from "react";
+import "./App.css";
+function App() {
+  return (
+    <form action="http://34.95.249.49/alunos/" method="POST">
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default App;
